965fa91927acd44b4ecc0eb12eb65a70c0264269
maelstrom <heap.tim@gmail.com>
2010-11-22 14:34:25 +1100

    Added the article title to the page title

--- templates/default.php
+++ templates/default.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
-		<title>Maelstroms Corner - Page</title>
+		<title>Maelstroms Corner<?php if (!empty($article['title']))  echo ' - ' . $article['title']; ?></title>
 
 		<link href="/css/page.css" type="text/css" rel="stylesheet" />
 		<script type="text/javascript" src="/js/common/mootools/mootools.js"></script>
